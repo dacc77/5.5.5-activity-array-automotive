1fc409eade8d0237418eb5cd5651bcee4cbe7e10
dacc77 <123707776+dacc77@users.noreply.github.com>
2023-07-22 22:57:49 -0700

    changed the vehicle.js

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,4 +1,5 @@
 //this includes the vehicle class as a module
+const { Vehicle } = require("./vehicle");
 const VehicleModule = require("./vehicleBaseClass")
 
 //this shows how to call from this module...
@@ -8,11 +9,72 @@ console.log(v.make)
 
 //After you write the derived Car class, you should test it out.
 
+
 //Note: You can code your derived Car class here or make a file named index.js and do it there.
 
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
+//create car class that extends vehicle.js
+// create constructor
+// add the superkeyword to call the parent class constructor
+//add properties 
+//maximumPassengers
+//passengers,numberOfWheels,maximumSpeed,fuel, scheduleService
+//add methods of psuedologic
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
 
+    selfService() {
+        if (this.mileage > 30000) {            
+            this.scheduleService = true
+            return this.scheduleService;                       
+        }
+    }
+
+    start() {
+        if (this.fuel > 0) {            
+            console.log("engine has started");
+            return this.started = true
+        } else {
+            console.log("no fuel");
+            return this.started = false;
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+
+
+}
+
+
+let myCar = new Car('mercury', 'rad_sedan', '2002', 'white', 50000)
+
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
 
 
 
@@ -27,9 +89,11 @@ console.log(v.make)
 //TO DO: Creating Instances and Testing Them
 
 //You can use the same instance "v" of the Vehicle class above for the base class.
-
+let v = new VehicleModule.Vehicle("Nissan", "Sedan", "2018", "blue", "124688");
+console.log(v.make)
 
 
 
 
 //Create at least two new instances of the Car class and test them here:
+
